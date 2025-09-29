23cd5b311e16d2b8ce0872d970ffcdf94518f4fa
h51 <stonemaomao8836@gmail.com>
2025-09-29 09:46:57 +0800

    change

---
+++ AICodeX/AICodeX/AICodeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  AICodeXApp.swift
+//  AICodeX
+//
+//  Created by admin on 2025/9/29.
+//
+
+import SwiftUI
+
+@main
+struct AICodeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
